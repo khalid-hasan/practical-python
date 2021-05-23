f55809c81f2aa58873bc4d9233e7e63b9140d3bf
Khalid Hasan <khalidibnehasan@gmail.com>
2021-05-23 00:20:49 -0230

    Solution to Exercise 1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -11,15 +11,23 @@ extra_payment = 1000
 extra_payment_duration = 12
 extra_payment_start_month  = 61
 extra_payment_end_month = 108
-
+overpayment_total_paid = 0
+overpayment_total_months = 0
 while principal > 0:
     if months >= extra_payment_start_month and months <= extra_payment_end_month:
         principal = principal - extra_payment
         total_paid = total_paid + extra_payment
+    
+    principal = principal * (1+rate/12) - payment
+    if(principal < 0):
+        overpayment_total_paid = total_paid
+        overpayment_total_months = months
+    total_paid = total_paid + payment
     months = months + 1
 
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
+    if (principal < 0):
+        total_paid = overpayment_total_paid
+        months = overpayment_total_months
     print(f'{months} {total_paid:0.2f} {principal:0.2f}')
     #Exercise 1.10 has already been implemented in the previous commit.
 
