2788696f94167a0dd8553fc3b2e81f7c59a476b3
Khalid Hasan <khalidibnehasan@gmail.com>
2021-05-24 01:31:56 -0230

    Solution to exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,23 +1,23 @@
 # pcost.py
 #
-# Exercise 1.31
+# Exercise 1.32
 
 import gzip
-
+import csv
 
 
 def portfolio_cost(filename):
         total_cost = 0
         f = open(filename, 'rt')
-        headers = next(f).split(',')
+        rows= csv.reader(f)
+        headers = next(rows)
 
-        for line in f:
+        for row in rows:
                 try:
-                        row = line.split(',')
                         total_cost = total_cost + (int(row[1]) * float(row[2]))
                         #print(row)
                 except ValueError:
-                        print('Data Missing', line)
+                        print('Data Missing', row)
         f.close()
         return total_cost
 
