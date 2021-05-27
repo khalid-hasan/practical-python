56a1f35799fc227ff9bfb43840139f9f0a4fb611
Khalid Hasan <khalidibnehasan@gmail.com>
2021-05-26 22:31:22 -0230

    Solution to Exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,28 @@
 # report.py
 #
 # Exercise 2.4
+
+import gzip
+import csv
+import sys
+
+
+def read_portfolio(filename):
+        portfolio = []
+        f = open(filename, 'rt')
+        rows= csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+                try:
+                        holding = (row[0], int(row[1]), float(row[2]))
+                        portfolio.append(holding)
+                except ValueError:
+                        print('Data Missing', row)
+        f.close()
+        return portfolio
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
