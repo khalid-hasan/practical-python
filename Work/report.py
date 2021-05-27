0b61cd5efb8940fab7a5be2e8a508b22b8e829b5
Khalid Hasan <khalidibnehasan@gmail.com>
2021-05-27 02:20:05 -0230

    Solution to Exercise 2.5

--- Work/report.py
+++ Work/report.py
@@ -8,21 +8,31 @@ import sys
 
 
 def read_portfolio(filename):
-        portfolio = []
+        dict_list = []
+        total = 0
         f = open(filename, 'rt')
         rows= csv.reader(f)
         headers = next(rows)
 
         for row in rows:
                 try:
-                        holding = (row[0], int(row[1]), float(row[2]))
-                        portfolio.append(holding)
+                        holding = {
+                            'name': row[0], 
+                            'shares': int(row[1]), 
+                            'prices': float(row[2])
+                        }
+                        dict_list.append(holding)
+                        total += int(row[1]) * float(row[2])
                 except ValueError:
                         print('Data Missing', row)
         f.close()
-        return portfolio
+        return [total, dict_list]
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
     filename = 'Data/portfolio.csv'
+
+cost = read_portfolio(filename)
+print('Total Cost', cost[0])
+print('Dictionary', cost[1])
