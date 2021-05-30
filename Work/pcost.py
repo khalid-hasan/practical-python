42bde7aa801721bcbee32e071f938cc6b3953039
Khalid Hasan <khalidibnehasan@gmail.com>
2021-05-30 20:00:27 -0230

    Solution to Exercise 3.14

--- Work/pcost.py
+++ Work/pcost.py
@@ -5,23 +5,15 @@
 import gzip
 import csv
 import sys
-
+import report
 
 def portfolio_cost(filename):
         total_cost = 0
-        f = open(filename, 'rt')
-        rows= csv.reader(f)
-        headers = next(rows)
-
-        for rowno, row in enumerate(rows, start=1):
-                record = dict(zip(headers, row))
-                try:
-                        nshares = int(record['shares'])
-                        price = float(record['price'])
-                        total_cost += nshares * price
-                except ValueError:
-                        print(f'Row {rowno}: Couldn\'t convert: {row}')
-        f.close()
+        portfolio = report.read_portfolio(filename)
+        for rows in portfolio:
+                nshares = int(rows['shares'])
+                price = float(rows['price'])
+                total_cost += nshares * price
         return total_cost
 
 if len(sys.argv) == 2:
