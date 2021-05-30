31b56f0f5acfedc824fc7724d1c7476efbaa213f
Khalid Hasan <khalidibnehasan@gmail.com>
2021-05-30 13:15:35 -0230

    Solution to Exercise 3.5

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,7 +4,7 @@
 
 import csv
 
-def parse_csv(filename, select=None):
+def parse_csv(filename, select=None, types=[str, int, float]):
 
     with open(filename) as f:
         rows = csv.reader(f)
@@ -23,11 +23,13 @@ def parse_csv(filename, select=None):
                 continue
             if indices:
                 row = [ row[index] for index in indices ]
+            if types:
+                row = [func(val) for func, val in zip(types, row) ]
 
             record = dict(zip(headers, row))
             records.append(record)
 
     return records
 
-portfolio = parse_csv('Data/portfolio.csv', ['name', 'shares'])
+portfolio = parse_csv('Data/portfolio.csv', ['name', 'shares'], types=[str])
 print(portfolio)
