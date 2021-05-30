d6a32f40697992fd90956755e5d8e35e8634b2e0
Khalid Hasan <khalidibnehasan@gmail.com>
2021-05-30 19:55:41 -0230

    Solution to Exercise 3.12

--- Work/report.py
+++ Work/report.py
@@ -5,62 +5,34 @@
 import gzip
 import csv
 import sys
+import fileparse
 
 
 def read_portfolio(filename):
-        dict_list = []
-        total = 0
-        f = open(filename, 'rt')
-        rows= csv.reader(f)
-        headers = next(rows)
 
-        for rowno, row in enumerate(rows, start=1):
-                record = dict(zip(headers, row))
-                try:
-                        holding = {
-                                'name': record['name'], 
-                                'shares': int(record['shares']), 
-                                'prices': float(record['price'])
-                        }
-                        dict_list.append(holding)
-                        total += int(row[1]) * float(row[2])
-                except ValueError:
-                        print(f'Row {rowno}: Couldn\'t convert: {row}')
-        f.close()
-        return [total, dict_list]
+        return fileparse.parse_csv(filename, select=['name','shares','price'], types=[str,int,float])
 
 def read_prices(filename):
-        dictionary = {}
-        f = open(filename, 'rt')
-        rows= csv.reader(f)
 
-        for row in rows:
-                try:
-                        dictionary[row[0]] = float(row[1])
-                        
-                except IndexError:
-                        pass
-        f.close()
-        return dictionary
+        return dict(fileparse.parse_csv(filename,types=[str,float], has_headers=False))
 
 def make_report(portfolio, prices_dictionary):
     data = []
     for row in portfolio:
         current_price = prices_dictionary[row['name']]
-        change = current_price - row['prices']
+        change = current_price - row['price']
         report = (row['name'], row['shares'], current_price, change)
         data.append(report)
     return data
 
 def portfolio_report(portfolio_filename, prices_filename):
         portfolio = read_portfolio(portfolio_filename)
-        print('Total Cost', portfolio[0])
-        print('Dictionary', portfolio[1])
+        print('Total Cost', portfolio)
 
         prices_dictionary = read_prices(prices_filename)
         print(prices_dictionary)
 
-        report = make_report(portfolio[1], prices_dictionary)
+        report = make_report(portfolio, prices_dictionary)
         headers = ('Name', 'Shares', 'Price', 'Change')
         print('%10s %10s %10s %10s' % headers)
         print(10*'-' + ' ' + 10*'-' + ' ' + 10*'-' + ' ' + 10*'-')
