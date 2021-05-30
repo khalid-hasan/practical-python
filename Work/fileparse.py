4289c8bf1caa24984925779f53a79d3140bb79d0
Khalid Hasan <khalidibnehasan@gmail.com>
2021-05-30 13:00:40 -0230

    Solution to Exercise 3.4

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,18 +4,30 @@
 
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename, select=None):
 
     with open(filename) as f:
         rows = csv.reader(f)
 
         headers = next(rows)
+
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+
         records = []
         for row in rows:
-            if not row:  
+            if not row:   
                 continue
+            if indices:
+                row = [ row[index] for index in indices ]
+
             record = dict(zip(headers, row))
             records.append(record)
 
     return records
 
+portfolio = parse_csv('Data/portfolio.csv', ['name', 'shares'])
+print(portfolio)
